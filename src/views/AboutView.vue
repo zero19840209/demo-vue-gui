54ab1e6871e04a8f7c5902fd85ba21d0d4bc46a2
zero <a0980866672@gmail.com>
2024-03-02 21:46:05 +0800

    修改

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,16 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <BaseCard></BaseCard>
 </template>
+<script>
+// @ is an alias to /src
+import BaseCard from '@/components/BaseCard.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    BaseCard
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <VForm v-slot="{ errors, values, validate }" @submit="onSubmit">
+
+      {{ errors }} {{ values }}
+
+      <div class="mb-3">
+
+        <label for="email" class="form-label">Email</label>
+
+        <VField id="email" name="email" type="email" class="form-control" :class="{ 'is-invalid': errors['email'] }"
+          placeholder="請輸入 Email" rules="email|required" v-model="user.email"></VField>
+
+        <v-error-message name="email" class="invalid-feedback"></v-error-message>
+
+      </div>
+
+      <button class="btn me-2 btn-outline-primary" type="button" @click="validate">驗證</button>
+
+      <button class="btn btn-primary" type="submit">Submit</button>
+
+    </VForm>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.user)
+    }
+  },
+  created () {
+    console.log(this)
   }
+
 }
 </script>
